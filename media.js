fb0132788ca233ed1b095d65d53c31277cff45e1
adcavalcant <aderc19@gmail.com>
2023-06-29 15:54:25 -0300

    Média

---
+++ media.js
@@ -0,0 +1,10 @@
+// const nota1 = 7;
+// const nota2 = 8.5;
+// const nota3 = 5;
+// const nota4 = 3.5;
+// const media = (nota1 + nota2 + nota3 + nota4) / 4;
+// console.log(media);
+
+const notas = [7, 8.5, 5, 3.5];
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length; //length pega o tamanho do array
+console.log(media);
