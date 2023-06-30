d2bc62e44db14e607f43c4d3f0ba7532f85bff96
adcavalcant <aderc19@gmail.com>
2023-06-30 10:52:30 -0300

    Array Multidimensional

---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,10 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias];
+console.log(listaDeAlunosEMedias);
+console.log(listaDeAlunosEMedias[0]);
+console.log(listaDeAlunosEMedias[1]);
+
+console.log(listaDeAlunosEMedias[0][2]);
+console.log(listaDeAlunosEMedias[1][2]);
