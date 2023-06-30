72d8cad61853857f504914e290b9ce4cd7a73b0a
adcavalcant <aderc19@gmail.com>
2023-06-30 10:43:18 -0300

    Método splice() - alterando o array

---
+++ atualizando-alunos.js
@@ -0,0 +1,5 @@
+const alunos = ["João", "Ana", "Caio", "Lara", "Marjorie", "Léo"];
+
+//remove 2 itens a partir do alunos[1] e adiciona o Rodrigo na posição alunos[1].
+alunos.splice(1, 2, "Rodrigo");
+console.log(alunos);
