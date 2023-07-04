37d37cbd9f25ba31e537a358090c62f9a18713df
adcavalcant <aderc19@gmail.com>
2023-07-04 16:25:38 -0300

    Removendo nomes repetidos da lista de alunos utilizando a Classe Set e operador de Espalhamento (...)

---
+++ removendo-elementos-repetidos.js
@@ -0,0 +1,6 @@
+const alunos = ["Ana", "Clara", "Maria", "Maria", "João", "João", "João"];
+
+const meuSet = new Set(alunos);
+const alunosAtualizados = [...meuSet];
+
+console.log(alunosAtualizados);
