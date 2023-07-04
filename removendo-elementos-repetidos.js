93ea052b3f2e68779fa5300eb97bc0ccfe1fbfc6
adcavalcant <aderc19@gmail.com>
2023-07-04 16:26:31 -0300

    ajuste

--- removendo-elementos-repetidos.js
+++ removendo-elementos-repetidos.js
@@ -1,6 +1,8 @@
 const alunos = ["Ana", "Clara", "Maria", "Maria", "João", "João", "João"];
 
 const meuSet = new Set(alunos);
+
+// const alunosAtualizados = [...new Set(nomes)];
 const alunosAtualizados = [...meuSet];
 
 console.log(alunosAtualizados);
